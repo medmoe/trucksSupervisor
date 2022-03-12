d3ad576c438532c91aa4748b0a247921b9961d5c
medmoe <med.seffah@gmail.com>
2022-03-11 20:55:26 -0500

    set up backend handlers

--- agent/models.py
+++ agent/models.py
@@ -1,3 +1,27 @@
+import datetime
+
 from django.db import models
 
+
 # Create your models here.
+
+
+class Entry(models.Model):
+    owner = models.ForeignKey('auth.User', related_name='agent', on_delete=models.CASCADE)
+    created = models.DateTimeField(default=datetime.datetime.now())
+    entry_id = models.AutoField(primary_key=True)
+    driver_name = models.CharField(max_length=100, default="", blank=True)
+    init_load = models.DecimalField(max_digits=6, decimal_places=2)
+    init_amount = models.DecimalField(max_digits=8, decimal_places=2)
+    init_km = models.DecimalField(max_digits=8, decimal_places=2)
+    fin_load = models.DecimalField(max_digits=6, decimal_places=2)
+    fin_amount = models.DecimalField(max_digits=8, decimal_places=2)
+    fin_km = models.DecimalField(max_digits=8, decimal_places=2)
+    status = models.BooleanField(default=True)
+    cause = models.TextField()
+
+    def __str__(self):
+        return self.driver_name
+
+    class Meta:
+        ordering = ['created']
--- agent/views.py
+++ agent/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from agent.models import Entry
+from agent.serializers import EntrySerializer
+from rest_framework import mixins
+from rest_framework import generics
+
 
 # Create your views here.
+
+class EntryList(generics.ListCreateAPIView):
+    queryset = Entry.objects.all()
+    serializer_class = EntrySerializer
+
+
+class EntryDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Entry.objects.all()
+    serializer_class = EntrySerializer
